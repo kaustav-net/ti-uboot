8de58a490721ec9c8b29a89db7416525df220e0c
Tom Rini <trini@ti.com>
2015-01-07 12:25:52 -0500

    dwc3: Fixups for SPL
    To enable DWC3 in SPL we need to list it in scripts/Makefile.spl.  In
    addition we needed to add a gadget_is_dwc3 define to gadget_chips.h
    because in SPL we garbage-collect uncalled functions and without this
    check we would remove all of the DWC3 functions.
    
    Signed-off-by: Tom Rini <trini@ti.com>

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -156,6 +156,12 @@
 #define gadget_is_fotg210(g)        0
 #endif
 
+#ifdef CONFIG_USB_DWC3_GADGET
+#define gadget_is_dwc3(g)	(!strcmp("dwc3-gadget", (g)->name))
+#else
+#define gadget_is_dwc3(g)	0
+#endif
+
 /*
  * CONFIG_USB_GADGET_SX2
  * CONFIG_USB_GADGET_AU1X00
@@ -223,5 +229,7 @@ static inline int usb_gadget_controller_number(struct usb_gadget *gadget)
 		return 0x21;
 	else if (gadget_is_fotg210(gadget))
 		return 0x22;
+	else if (gadget_is_dwc3(gadget))
+		return 0x23;
 	return -ENOENT;
 }
