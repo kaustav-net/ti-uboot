1e436b73022d4fd3611d6c7be57c488c662e94e7
Nishanth Menon <nm@ti.com>
2015-02-13 17:56:17 -0600

    ARM: OMAP5 / DRA7: Setup L2 Aux Control Register with recommended configuration
    Update to existing recommendation for L2ACTLR configuration to prevent
    system instability and optimize performance.
    
    These apply to both OMAP5 and DRA7.
    
    Reported-by: Vivek Chengalvala <vchengalvala@ti.com>
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/include/asm/arch-omap5/sys_proto.h
+++ arch/arm/include/asm/arch-omap5/sys_proto.h
@@ -69,6 +69,7 @@ void force_emif_self_refresh(void);
 void get_ioregs(const struct ctrl_ioregs **regs);
 void srcomp_enable(void);
 void setup_warmreset_time(void);
+void omap_smc1(u32 service, u32 val);
 
 static inline u32 div_round_up(u32 num, u32 den)
 {
@@ -79,4 +80,7 @@ static inline u32 usec_to_32k(u32 usec)
 {
 	return div_round_up(32768 * usec, 1000000);
 }
+
+#define OMAP5_SERVICE_L2ACTLR_SET    0x104
+
 #endif
