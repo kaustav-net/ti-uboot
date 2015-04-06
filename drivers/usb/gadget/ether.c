kaustav-net/ti-uboot

commit 0db60451ac0a99098f9366dc34d9f372df828299

usb: gadget: ether: initialize and cleanup gadget from ether gadget
Initializing the dwc3 gadget driver and not disabling it results in
dwc3 being in a state which prevents system from suspending while doing
a PM in the linux kernel. By moving the initialization function
to the ether gadget gives a place where cleanup function for
dwc3 driver can be invoked.

Reported-by: Keerthy <j-keerthy@ti.com>
Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

drivers/usb/gadget/ether.c
@@ -2337,6 +2337,7 @@ static int usb_eth_init(struct eth_device *netdev, bd_t *bd)
 	unsigned long ts;
 	unsigned long timeout = USB_CONNECT_TIMEOUT;
 
+	board_usb_init(0, USB_INIT_DEVICE);
 	if (!netdev) {
 		error("received NULL ptr");
 		goto fail;
@@ -2521,6 +2522,7 @@ void usb_eth_halt(struct eth_device *netdev)
 	}
 
 	usb_gadget_unregister_driver(&eth_driver);
+	board_usb_cleanup(0, USB_INIT_DEVICE);
 }
 
 static struct usb_gadget_driver eth_driver = {

board/ti/am43xx/board.c
@@ -609,9 +609,6 @@ int board_late_init(void)
 	safe_string[sizeof(header.version)] = 0;
 	setenv("board_rev", safe_string);
 #endif
-#ifdef CONFIG_USB_ETHER
-	board_usb_init(0, USB_INIT_DEVICE);
-#endif
 	return 0;
 }
 #endif

commit 2a056d92150b85377f7329a3450da7805748f6a7

usb: gadget: ether: fix build breakage
Building with ARM-based am43xx_evm_config results in

/datalocal/Sekhar/u-boot-am33x/drivers/usb/gadget/ether.c:2340:20: error: ‘USB_INIT_DEVICE’ undeclared (first use in this function)

Fix this.

Fixes: 0db60451ac0a ("usb: gadget: ether: initialize and cleanup gadget from ether gadget")
Signed-off-by: Sekhar Nori <nsekhar@ti.com>

drivers/usb/gadget/ether.c
@@ -15,6 +15,7 @@
 #include <linux/usb/cdc.h>
 #include <linux/usb/gadget.h>
 #include <net.h>
+#include <usb.h>
 #include <malloc.h>
 #include <linux/ctype.h>
 