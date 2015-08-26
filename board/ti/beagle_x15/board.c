kaustav-net/ti-uboot

commit ae66768f7de13abef62205da373f6912f6807789

ARM: BeagleBoard-X15: Enable VTT regulator
BeagleBoard-X15 uses a vtt regulator for DDR3 termination
and this is controlled by gpio7_11. Configuring gpio7_11.

Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

board/ti/beagle_x15/board.c
@@ -14,6 +14,8 @@
 #include <usb.h>
 #include <asm/omap_common.h>
 #include <asm/emif.h>
+#include <asm/gpio.h>
+#include <asm/arch/gpio.h>
 #include <asm/arch/clock.h>
 #include <asm/arch/dra7xx_iodelay.h>
 #include <asm/arch/sys_proto.h>
@@ -29,6 +31,9 @@
 #endif
 
 DECLARE_GLOBAL_DATA_PTR;
+
+/* GPIO 7_11 */
+#define GPIO_DDR_VTT_EN 203
 
 const struct omap_sysinfo sysinfo = {
 	"Board: BeagleBoard x15\n"
@@ -410,7 +415,22 @@ int board_usb_init(int index, enum usb_init_type init)
 {
 	setbits_le32((*prcm)->cm_l3init_usb_otg_ss_clkctrl,
 			OTG_SS_CLKCTRL_MODULEMODE_HW | OPTFCLKEN_REFCLK960M);
+}
 
+#ifdef CONFIG_BOARD_EARLY_INIT_F
+/* VTT regulator enable */
+static inline void vtt_regulator_enable(void)
+{
+	if (omap_hw_init_context() == OMAP_INIT_CONTEXT_UBOOT_AFTER_SPL)
+		return;
+
+	gpio_request(GPIO_DDR_VTT_EN, "ddr_vtt_en");
+	gpio_direction_output(GPIO_DDR_VTT_EN, 1);
+}
+
+int board_early_init_f(void)
+{
+	vtt_regulator_enable();
 	return 0;
 }
 #endif

include/configs/beagle_x15.h
@@ -18,6 +18,8 @@
 #define CONFIG_IODELAY_RECALIBRATION
 #endif
 
+#define CONFIG_BOARD_EARLY_INIT_F
+
 #define CONFIG_NR_DRAM_BANKS		2
 
 #define CONFIG_ENV_SIZE			(64 << 10)

commit 8a3da940437a149380d9c9303f8f0275d9b4b856

ARM: beagleboard-X15: fix build breakage
commit ae66768f7de1 ("ARM: BeagleBoard-X15: Enable VTT regulator")
broke beagle_x15 build by leaving a unterminated #ifdef. fix it.

Signed-off-by: Nishanth Menon <nm@ti.com>

board/ti/beagle_x15/board.c
@@ -415,7 +415,9 @@ int board_usb_init(int index, enum usb_init_type init)
 {
 	setbits_le32((*prcm)->cm_l3init_usb_otg_ss_clkctrl,
 			OTG_SS_CLKCTRL_MODULEMODE_HW | OPTFCLKEN_REFCLK960M);
+	return 0;
 }
+#endif
 
 #ifdef CONFIG_BOARD_EARLY_INIT_F
 /* VTT regulator enable */