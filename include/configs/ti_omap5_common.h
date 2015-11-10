f3171e0a0d41bc79f819a1b85563ef7c643bf59b
Steve Kipisz <s-kipisz2@ti.com>
2015-11-09 18:34:49 -0600

    board: ti: am57xx: Add support for the am571x idk
    The AM571x Industrial Development Kit (IDK) is a board based on TI's
    AM571x SoC which has a single core 1.5GHz Cortex-A15processor. This
    board is a development platform for the Industrial Market with:
    
    - 1GB of DDR3L
    - Dual 1Gbps Ethernet
    - HDMI
    - PRU-ICSS
    - uSD
    - 16GB eMMC
    - CAN
    - RS-485
    - PCIe
    - USB3.0
    - Video Input Port
    - Industrial IO port and expansion connector
    
    The PRU/ICSS will be supported by 3rd party software for EtherCat,
    Profibus, and other Industrial protocols.
    
    The link to the data sheet and TRM can be found here:
    http://www.ti.com/product/AM5718
    
    Signed-off-by: Steve Kipisz <s-kipisz2@ti.com>
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- include/configs/ti_omap5_common.h
+++ include/configs/ti_omap5_common.h
@@ -184,6 +184,8 @@
 			"setenv fdtfile am57xx-evm.dtb; fi;" \
 		"if test $board_name = am572x_idk; then " \
 			"setenv fdtfile am572x-idk.dtb; fi;" \
+		"if test $board_name = am571x_idk; then " \
+			"setenv fdtfile am571x-idk.dtb; fi;" \
 		"if test $fdtfile = undefined; then " \
 			"echo WARNING: Could not determine device tree to use; fi; \0" \
 	"loadfdt=load mmc ${bootpart} ${fdtaddr} ${bootdir}/${fdtfile};\0" \
