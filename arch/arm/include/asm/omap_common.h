1590744c7b608137095bd6a7f0296deac7ec3de8
Steve Kipisz <s-kipisz2@ti.com>
2015-11-09 18:34:40 -0600

    ARM: OMAP4/5: Centralize gpi2c_init
    Centralize gpi2c_init into omap_common from the sys_proto header so
    that the information can be reused across SoCs.
    
    Signed-off-by: Steve Kipisz <s-kipisz2@ti.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Lokesh Vutla <lokeshvutla@ti.com>
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/include/asm/omap_common.h
+++ arch/arm/include/asm/omap_common.h
@@ -616,6 +616,9 @@ void enable_edma3_clocks(void);
 void disable_edma3_clocks(void);
 #endif
 
+/* Initialize general purpose I2C(0) on the SoC */
+void gpi2c_init(void);
+
 /* ABB */
 #define OMAP_ABB_NOMINAL_OPP		0
 #define OMAP_ABB_FAST_OPP		1
