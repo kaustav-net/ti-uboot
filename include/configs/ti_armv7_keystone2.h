kaustav-net/ti-uboot

commit 123e72bd60a93834b83c4280c105b74c4ad9a3f8

ARM: keystone: K2G: Add PMMC remoteproc device
Texas Instruments K2G family of processors use PMMC (Power Management
Micro-Controller) to provide advanced power functionality to High
Level Operating Systems such as Linux.

Describe the same and enabled K2G-evm to start using the same

Signed-off-by: Nishanth Menon <nm@ti.com>

include/configs/ti_armv7_keystone2.h
@@ -216,6 +216,16 @@
 /* EDMA3 */
 #define CONFIG_TI_EDMA3
 
+#define DEFAULT_PMMC_BOOT_ENV						\
+	"name_pmmc=pmmc-firmware.bin\0"					\
+	"dev_pmmc=0\0"							\
+	"get_pmmc_net=dhcp ${loadaddr} ${tftp_root}/${name_pmmc}\0"	\
+	"get_pmmc_mmc=load mmc ${bootpart} ${loadaddr} ${bootdir}/${name_pmmc}\0"\
+	"get_pmmc_ubi=ubifsload ${loadaddr} ${name_pmmc}\0"		\
+	"run_pmmc=rproc init; rproc list; "				\
+		"rproc load ${dev_pmmc} ${loadaddr} 0x${filesize}; "	\
+		"rproc start ${dev_pmmc}\0"				\
+
 #define CONFIG_EXTRA_ENV_SETTINGS					\
 	DEFAULT_LINUX_BOOT_ENV						\
 	CONFIG_EXTRA_ENV_KS2_BOARD_SETTINGS				\
@@ -264,9 +274,11 @@
 	"mtdparts=mtdparts=davinci_nand.0:"				\
 		"1024k(bootloader)ro,512k(params)ro,-(ubifs)\0"
 
+#ifndef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND						\
 	"run init_${boot} get_fdt_${boot} get_mon_${boot} "		\
 		"get_kern_${boot} run_mon run_kern"
+#endif
 
 #define CONFIG_BOOTARGS							\
 

arch/arm/mach-keystone/include/mach/hardware-k2g.h
@@ -71,4 +71,7 @@
 #define K2G_GPIO_DIR_OFFSET		0x0
 #define K2G_GPIO_SETDATA_OFFSET		0x8
 
+/* PMMC */
+#define K2G_PMMC_SLAVE_BASE		0x02900000
+
 #endif /* __ASM_ARCH_HARDWARE_K2G_H */

arch/arm/mach-keystone/keystone.c
@@ -13,6 +13,8 @@
 #include <asm/arch/psc_defs.h>
 #include <asm/arch/hardware.h>
 #include <asm/arch/hardware.h>
+#include <remoteproc.h>
+#include <dm/platform_data/remoteproc_ti_power_proc.h>
 
 /**
  * cpu_to_bus - swap bytes of the 32-bit data if the device is BE
@@ -85,3 +87,22 @@ int misc_init_r(void)
 
 	return 0;
 }
+
+#if !(defined (CONFIG_SPL_BUILD) || defined (CONFIG_OF_CONTROL)) && \
+    defined (CONFIG_REMOTEPROC_TI_POWER)
+struct ti_powerproc_platdata  pmmc_driverdata = {
+	.slave_base = K2G_PMMC_SLAVE_BASE,
+	.module = KS2_LPSC_PMMC,
+};
+
+struct dm_rproc_uclass_pdata pmmc_data = {
+	.name = "PMMC",
+	.mem_type = RPROC_INTERNAL_MEMORY_MAPPED,
+	.driver_plat_data = &pmmc_driverdata,
+};
+
+U_BOOT_DEVICE(pmmc) = {
+	.name = "ti_power_proc",
+	.platdata = &pmmc_data,
+};
+#endif

configs/k2g_evm_defconfig
@@ -6,3 +6,6 @@ CONFIG_SPL=y
 # CONFIG_CMD_FLASH is not set
 # CONFIG_CMD_SETEXPR is not set
 CONFIG_SPI_FLASH=y
+CONFIG_DM=y
+CONFIG_REMOTEPROC_TI_POWER=y
+CONFIG_CMD_REMOTEPROC=y

include/configs/k2g_evm.h
@@ -17,6 +17,7 @@
 /* U-Boot general configuration */
 #define CONFIG_EXTRA_ENV_KS2_BOARD_SETTINGS				\
 	DEFAULT_MMC_TI_ARGS						\
+	DEFAULT_PMMC_BOOT_ENV						\
 	"console=ttyS0,115200n8\0"					\
 	"bootpart=0:2\0"						\
 	"bootdir=/boot\0"						\
@@ -32,6 +33,11 @@
 	"get_kern_mmc=load mmc ${bootpart} ${loadaddr} "		\
 		"${bootdir}/${name_kern}\0"				\
 	"get_mon_mmc=load mmc ${bootpart} ${addr_mon} ${bootdir}/${name_mon}\0"\
+
+#define CONFIG_BOOTCOMMAND						\
+	"run init_${boot} get_pmmc_${boot} run_pmmc "			\
+	"get_fdt_${boot} get_mon_${boot} get_kern_${boot} "		\
+	"run_mon run_kern"
 
 #include <configs/ti_armv7_keystone2.h>
 

commit fb6b8b31f818d91e1b457938ac698319613fa9c5

configs: ti_armv7_keystone2: Get pmmc binary from {bootdir} in UBI
The PMMC firmware binary is currently fetched from different locations
between the MMC boot and UBI boot modes for K2G SoC. Fix up the
get_pmmc_ubi env variable so that the PMMC firmware binary location
in UBI mode matches that of MMC boot. This allows the file system
to place the PMMC binary at the same place as the kernel, DT blob
and boot monitor binaries, and make it identical for MMC and UBI
boot modes.

Fixes: 123e72bd60a9 ("ARM: keystone: K2G: Add PMMC remoteproc device")
Signed-off-by: Suman Anna <s-anna@ti.com>
Acked-by: Nishanth Menon <nm@ti.com>
Acked-by: Denys Dmytriyenko <denys@ti.com>

include/configs/ti_armv7_keystone2.h
@@ -208,7 +208,7 @@
 	"dev_pmmc=0\0"							\
 	"get_pmmc_net=dhcp ${loadaddr} ${tftp_root}/${name_pmmc}\0"	\
 	"get_pmmc_mmc=load mmc ${bootpart} ${loadaddr} ${bootdir}/${name_pmmc}\0"\
-	"get_pmmc_ubi=ubifsload ${loadaddr} ${name_pmmc}\0"		\
+	"get_pmmc_ubi=ubifsload ${loadaddr} ${bootdir}/${name_pmmc}\0"	\
 	"run_pmmc=rproc init; rproc list; "				\
 		"rproc load ${dev_pmmc} ${loadaddr} 0x${filesize}; "	\
 		"rproc start ${dev_pmmc}\0"				\