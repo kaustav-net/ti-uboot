kaustav-net/ti-uboot

commit c83bd9820e9ae9a5345c582009a27b8aa746c5a6

configs: k2g_evm/keystone2: Rename pmmc binary to be generic
To stay in sync with future variations of SoC, firmware is now renamed
to ti-sci-firmware-<soc_variant>.bin. Update the configuration for the
same.

Signed-off-by: Nishanth Menon <nm@ti.com>
Acked-by: Russ Dill <russ.dill@ti.com>

include/configs/k2g_evm.h
@@ -28,6 +28,7 @@
 	"name_mon=skern-k2g.bin\0"					\
 	"name_ubi=k2g-evm-ubifs.ubi\0"					\
 	"name_uboot=u-boot-spi-k2g-evm.gph\0"				\
+	"soc_variant=k2g\0"						\
 	"init_mmc=run args_all args_mmc\0"				\
 	"get_fdt_mmc=load mmc ${bootpart} ${fdtaddr} ${bootdir}/${name_fdt}\0"\
 	"get_kern_mmc=load mmc ${bootpart} ${loadaddr} "		\

include/configs/ti_armv7_keystone2.h
@@ -204,7 +204,7 @@
 #define CONFIG_TI_EDMA3
 
 #define DEFAULT_PMMC_BOOT_ENV						\
-	"name_pmmc=pmmc-firmware.bin\0"					\
+	"name_pmmc=ti-sci-firmware-${soc_variant}.bin\0"		\
 	"dev_pmmc=0\0"							\
 	"get_pmmc_net=dhcp ${loadaddr} ${tftp_root}/${name_pmmc}\0"	\
 	"get_pmmc_mmc=load mmc ${bootpart} ${loadaddr} ${bootdir}/${name_pmmc}\0"\

commit 5922e09363b1449ba558fd1dfcd527c71119d0ee

configs: k2g_evm/keyston2: Fix pmmc binary name update
When we use the following in bootargs:
v1=abc
v2=123-${v1}
echo ${v2}
we get 123-${v1}
This was discussed in https://patchwork.ozlabs.org/patch/552864/
and Wolfgang disagrees that we should do recursive variable parsing.

So, fix it up to parse during run command itself. Kind of a little
convoluted, but anyways..

Fixes: c83bd9820e9a ("configs: k2g_evm/keystone2: Rename pmmc binary to be generic")
Signed-off-by: Nishanth Menon <nm@ti.com>
Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

include/configs/k2g_evm.h
@@ -38,7 +38,7 @@
 
 #define CONFIG_BOOTCOMMAND						\
 	"run envboot; "							\
-	"run init_${boot} get_pmmc_${boot} run_pmmc "			\
+	"run set_name_pmmc init_${boot} get_pmmc_${boot} run_pmmc "	\
 	"get_fdt_${boot} get_mon_${boot} get_kern_${boot} "		\
 	"run_mon run_kern"
 

include/configs/ti_armv7_keystone2.h
@@ -213,7 +213,7 @@
 		"run set_rd_spec\0"					\
 
 #define DEFAULT_PMMC_BOOT_ENV						\
-	"name_pmmc=ti-sci-firmware-${soc_variant}.bin\0"		\
+	"set_name_pmmc=setenv name_pmmc ti-sci-firmware-${soc_variant}.bin\0" \
 	"dev_pmmc=0\0"							\
 	"get_pmmc_net=dhcp ${loadaddr} ${tftp_root}/${name_pmmc}\0"	\
 	"get_pmmc_ramfs=run get_pmmc_net\0"				\