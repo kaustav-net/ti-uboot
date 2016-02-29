kaustav-net/ti-uboot

commit 894e719aadca2ff741682a1dffc5534599558794

ti: AM43xx: board: Detect AM43xx HS EVM
Adds code to detect AM43xx HS EVMS - the string in the
I2C EEPROM for HS EVMs differs from GP EVMs.

Modifies findfdt command to pick up am437x-gp-evm.dtb for
the HS EVMs also, as the boards are similar except for
some security specific changes around power supply and
enclosure protection.

Signed-off-by: Madan Srinivas <madans@ti.com>
Signed-off-by: Daniel Allred <d-allred@ti.com>

board/ti/am43xx/board.h
@@ -37,14 +37,21 @@ static inline int board_is_idk(void)
 	return board_ti_is("AM43_IDK");
 }
 
+static inline int board_is_hsevm(void)
+{
+	return board_ti_is("AM43XXHS");
+}
+
 static inline int board_is_evm_14_or_later(void)
 {
-	return board_is_gpevm() && strncmp("1.4", board_ti_get_rev(), 3) <= 0;
+	return (board_is_gpevm() || board_is_hsevm()) &&
+		strncmp("1.4", board_ti_get_rev(), 3) <= 0;
 }
 
 static inline int board_is_evm_12_or_later(void)
 {
-	return board_is_gpevm() && strncmp("1.2", board_ti_get_rev(), 3) <= 0;
+	return (board_is_gpevm() || board_is_hsevm()) &&
+		strncmp("1.2", board_ti_get_rev(), 3) <= 0;
 }
 
 void enable_uart0_pin_mux(void);

include/configs/am43xx_evm.h
@@ -301,6 +301,8 @@
 			"setenv fdtfile am43x-epos-evm.dtb; fi; " \
 		"if test $board_name = AM43__GP; then " \
 			"setenv fdtfile am437x-gp-evm.dtb; fi; " \
+		"if test $board_name = AM43XXHS; then " \
+			"setenv fdtfile am437x-gp-evm.dtb; fi; " \
 		"if test $board_name = AM43__SK; then " \
 			"setenv fdtfile am437x-sk-evm.dtb; fi; " \
 		"if test $board_name = AM43_IDK; then " \

commit e7e7e32e6d9e1962dd8503711fb2dace3c79cfd6

ti: AM437x: Fix HS-device variant related board init
With the introduction of support for the high-security (HS) variant of
the AM437x GPEVM a function to identify this EVM was introduced in commit
894e719aadca2f ("ti: AM43xx: board: Detect AM43xx HS EVM"). What was missing
however was to update all AM437x GPEVM related board init code to also get
invoked in case of using this newly added EVM. In order to arrive at an
easy to maintain solution moving forward in addition to fixing the issue
at hand, introduce a board detection function that does not distinguish
between GPEVM and HS EVM for cases where this is not needed, and utilize
this function in all applicable places.

The missing invocation of board init code manifests itself in significantly
reduced memory bandwidth observable during benchmarking but also
generating issues with display output.

Fixes: 894e719aadca2f ("ti: AM43xx: board: Detect AM43xx HS EVM")
Signed-off-by: Andreas Dannenberg <dannenberg@ti.com>
Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

board/ti/am43xx/board.c
@@ -341,7 +341,7 @@ const struct dpll_params *get_dpll_ddr_params(void)
 
 	if (board_is_eposevm())
 		return &epos_evm_dpll_ddr[ind];
-	else if (board_is_gpevm() || board_is_sk())
+	else if (board_is_evm() || board_is_sk())
 		return &gp_evm_dpll_ddr;
 	else if (board_is_idk())
 		return &idk_dpll_ddr;
@@ -585,7 +585,7 @@ u32 rtc_only_get_board_type(void)
 		return RTC_BOARD_EVM14;
 	else if (board_is_evm_12_or_later())
 		return RTC_BOARD_EVM12;
-	else if (board_is_gpevm())
+	else if (board_is_evm())
 		return RTC_BOARD_GPEVM;
 	else if (board_is_sk())
 		return RTC_BOARD_SK;
@@ -612,7 +612,7 @@ void sdram_init(void)
 		enable_vtt_regulator();
 		config_ddr(0, &ioregs_ddr3, NULL, NULL,
 			   &ddr3_emif_regs_400Mhz_beta, 0);
-	} else if (board_is_gpevm()) {
+	} else if (board_is_evm()) {
 		enable_vtt_regulator();
 		config_ddr(0, &ioregs_ddr3, NULL, NULL,
 			   &ddr3_emif_regs_400Mhz, 0);

board/ti/am43xx/board.h
@@ -42,16 +42,19 @@ static inline int board_is_hsevm(void)
 	return board_ti_is("AM43XXHS");
 }
 
+static inline int board_is_evm(void)
+{
+	return board_is_gpevm() || board_is_hsevm();
+}
+
 static inline int board_is_evm_14_or_later(void)
 {
-	return (board_is_gpevm() || board_is_hsevm()) &&
-		strncmp("1.4", board_ti_get_rev(), 3) <= 0;
+	return board_is_evm() && strncmp("1.4", board_ti_get_rev(), 3) <= 0;
 }
 
 static inline int board_is_evm_12_or_later(void)
 {
-	return (board_is_gpevm() || board_is_hsevm()) &&
-		strncmp("1.2", board_ti_get_rev(), 3) <= 0;
+	return board_is_evm() && strncmp("1.2", board_ti_get_rev(), 3) <= 0;
 }
 
 void enable_uart0_pin_mux(void);

board/ti/am43xx/mux.c
@@ -126,7 +126,7 @@ void enable_board_pin_mux(void)
 	configure_module_pin_mux(i2c0_pin_mux);
 	configure_module_pin_mux(mdio_pin_mux);
 
-	if (board_is_gpevm()) {
+	if (board_is_evm()) {
 		configure_module_pin_mux(gpio5_7_pin_mux);
 		configure_module_pin_mux(rgmii1_pin_mux);
 #if defined(CONFIG_NAND)