5756fa3303c64aa780f263bbb5d227e9bffb7c12
Srinivas, Madan <madans@ti.com>
2016-09-12 11:30:52 -0500

    arm: mach-keystone: config.mk: Adds support for secure, images on K2
    Adds an additional image type needed for supporting secure keystone
    devices. The build generates u-boot_HS_MLO which can be used to boot
    from all media on secure keystone devices.
    
    Signed-off-by: Madan Srinivas <madans@ti.com>
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- arch/arm/mach-keystone/config.mk
+++ arch/arm/mach-keystone/config.mk
@@ -5,8 +5,14 @@
 # SPDX-License-Identifier:     GPL-2.0+
 #
 
+include  $(srctree)/$(CPUDIR)/omap-common/config_secure.mk
+
 ifndef CONFIG_SPL_BUILD
+ifeq ($(CONFIG_TI_SECURE_DEVICE),y)
+ALL-y += u-boot_HS_MLO
+else
 ALL-y += MLO
+endif
 endif
 
 MKIMAGEFLAGS_u-boot-spl.gph = -A $(ARCH) -T gpimage -C none \
