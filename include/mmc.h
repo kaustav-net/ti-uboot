1d10d09f553a8eccd352112f69fd08155847906e
Kishon Vijay Abraham I <kishon@ti.com>
2017-02-08 22:20:29 +0530

    dm: mmc: Add a library function to parse generic dt binding
    Add a new function to parse host controller dt node and
    set mmc_config. This function can be used by mmc controller
    drivers to set the generic mmc_config.
    This function can be extended to set other UHS mode caps
    once UHS mode support is added.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- include/mmc.h
+++ include/mmc.h
@@ -541,6 +541,7 @@ int mmc_unbind(struct udevice *dev);
 int mmc_initialize(bd_t *bis);
 int mmc_init(struct mmc *mmc);
 int mmc_send_tuning(struct mmc *mmc, u32 opcode, int *cmd_error);
+int mmc_of_parse(const void *fdt, int node, struct mmc_config *cfg);
 int mmc_read(struct mmc *mmc, u64 src, uchar *dst, int size);
 void mmc_set_clock(struct mmc *mmc, uint clock);
 struct mmc *find_mmc_device(int dev_num);
