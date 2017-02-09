cd2c71d3f0132427ce386f1fbb8fb4da0f6e489b
Franklin S Cooper Jr <fcooper@ti.com>
2017-02-09 13:35:28 -0600

    ARM: k2g: Use board detection to wrap K2G GP specific calls
    Certain peripherals used by K2G GP aren't used on K2G ICE evm. Or
    configuration is slightly different. Therefore, use board detection to
    deal with these variations.
    
    Signed-off-by: Franklin S Cooper Jr <fcooper@ti.com>

--- board/ti/ks2_evm/board_k2g.c
+++ board/ti/ks2_evm/board_k2g.c
@@ -112,7 +112,9 @@ int board_mmc_init(bd_t *bis)
 		return -1;
 	}
 
-	omap_mmc_init(0, 0, 0, -1, -1);
+	if (board_is_k2g_gp())
+		omap_mmc_init(0, 0, 0, -1, -1);
+
 	omap_mmc_init(1, 0, 0, -1, -1);
 	return 0;
 }
@@ -162,11 +164,13 @@ int embedded_dtb_select(void)
 
 	k2g_reset_mux_config();
 
-	/* deassert FLASH_HOLD */
-	clrbits_le32(K2G_GPIO1_BANK2_BASE + K2G_GPIO_DIR_OFFSET,
-		     BIT(9));
-	setbits_le32(K2G_GPIO1_BANK2_BASE + K2G_GPIO_SETDATA_OFFSET,
-		     BIT(9));
+	if (board_is_k2g_gp()) {
+		/* deassert FLASH_HOLD */
+		clrbits_le32(K2G_GPIO1_BANK2_BASE + K2G_GPIO_DIR_OFFSET,
+			     BIT(9));
+		setbits_le32(K2G_GPIO1_BANK2_BASE + K2G_GPIO_SETDATA_OFFSET,
+			     BIT(9));
+	}
 
 	if (psc_enable_module(KS2_LPSC_GPMC))
 		printf("%s can't enable gpmc\n", __func__);
