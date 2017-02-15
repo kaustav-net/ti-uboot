cb1713974b29351c088506f4a4f442ca51e3bf23
Vitaly Andrianov <vitalya@ti.com>
2017-02-15 16:22:46 -0500

    arm: mach-keystone: Implements FIT post-processing call for keystone SoCs
    This commit implements the board_fit_image_post_process() function for
    the keystone architecture. This function calls into the secure boot
    monitor for secure authentication/decryption of the image. All needed
    work is handled by the boot monitor and, depending on the keystone
    platform, the security functions may be offloaded to other secure
    processing elements in the SoC.
    
    The boot monitor acts as the gateway to these secure functions and the
    boot monitor for secure devices is available as part of the SECDEV
    package for KS2. For more details refer doc/README.ti-secure
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Madan Srinivas <madans@ti.com>

--- arch/arm/mach-keystone/mon.c
+++ arch/arm/mach-keystone/mon.c
@@ -61,3 +61,62 @@ int mon_power_off(int core_id)
 		: "cc", "r0", "r1", "memory");
 	return  result;
 }
+
+#ifdef CONFIG_TI_SECURE_DEVICE
+#define KS2_HS_SEC_HEADER_LEN	0x60
+#define KS2_AUTH_CMD		130
+
+/**
+ * k2_hs_bm_auth() - Invokes security functions using a
+ * proprietary TI interface. This binary and source for
+ * this is available in the secure development package or
+ * SECDEV. For details on how to access this please refer
+ * doc/README.ti-secure
+ *
+ * @cmd: Secure monitor command
+ * @arg1: Argument for command
+ *
+ * returns non-zero value on success, zero on error
+ */
+static int k2_hs_bm_auth(int cmd, void *arg1)
+{
+	int result;
+
+	asm volatile (
+		"stmfd  r13!, {r4-r12, lr}\n"
+		"mov r0, %1\n"
+		"mov r1, %2\n"
+		"smc #2\n"
+		"ldmfd r13!, {r4-r12, lr}\n"
+		: "=&r" (result)
+		: "r" (cmd), "r" (arg1)
+		: "cc", "r0", "r1", "memory");
+
+	return  result;
+}
+
+static void k2_hs_auth(void *addr)
+{
+	int ret = 0;
+
+	ret = k2_hs_bm_auth(KS2_AUTH_CMD, addr);
+	if (ret == 0)
+		hang();
+}
+
+void board_fit_image_post_process(void **p_image, size_t *p_size)
+{
+	void *dst = *p_image;
+	void *src = dst + KS2_HS_SEC_HEADER_LEN;
+
+	k2_hs_auth(*p_image);
+
+	/*
+	* Overwrite the image headers  after authentication
+	* and decryption. Update size to reflect removal
+	* of header.
+	*/
+	*p_size -= KS2_HS_SEC_HEADER_LEN;
+	memcpy(dst, src, *p_size);
+}
+#endif
