4113ddaaf1dac2993a80678386c9f5b1c6eaf352
Andrew F. Davis <afd@ti.com>
2017-02-17 13:18:49 -0600

    ti_armv7_common: env: Change FIT image name to match build name
    The most common name for a FIT image containing a bootable kernel is
    "fitImage", as our builds now use this name also, change this to the
    default in our U-Boot environment.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>

--- include/configs/ti_armv7_common.h
+++ include/configs/ti_armv7_common.h
@@ -121,7 +121,7 @@
 #define DEFAULT_FIT_TI_ARGS \
 	"boot_fit=0\0" \
 	"fit_loadaddr=0x88000000\0" \
-	"fit_bootfile=fitImage.itb\0" \
+	"fit_bootfile=fitImage\0" \
 	"update_to_fit=setenv loadaddr ${fit_loadaddr}; setenv bootfile ${fit_bootfile}\0" \
 	"args_fit=setenv bootargs console=${console} \0" \
 	"loadfit=run args_fit; bootm ${loadaddr}#${fdtfile};\0" \
