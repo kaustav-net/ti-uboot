kaustav-net/ti-uboot

commit 7a0a68ac5b5b740a69a70c642530f5e470b1c23b

cmd: mmc: make command "mmc info" show the statistics
display the current tranfers statistics along with ther mmc information.

Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

cmd/mmc.c
@@ -12,6 +12,42 @@
 
 static int curr_device = -1;
 
+
+static char *sprintf_speed(char *buf, ulong sz, uint ms)
+{
+	uint64_t kbytes_per_sec = ((sz >> 10) * 1000) / ms;
+	uint64_t mb_per_sec = kbytes_per_sec >> 10;
+	uint32_t remain = ((kbytes_per_sec - (mb_per_sec << 10)) * 100) >> 10;
+	if (ms && mb_per_sec)
+		sprintf(buf, "%llu.%02u MB/s", mb_per_sec, remain);
+	else if (ms && kbytes_per_sec)
+		sprintf(buf, "%llu KB/s", kbytes_per_sec);
+	else
+		strcpy(buf, "### MB/s");
+	return buf;
+}
+
+static void show_stats(struct mmc *mmc)
+{
+	char buf[50];
+	struct mmc_statistics *s;
+	s = &mmc->rd_stats;
+
+	printf("read %s. error %d/%d (%d%%)\n",
+	       sprintf_speed(buf,
+			     s->total_sz *  mmc->read_bl_len,
+			     s->total_time),
+		s->errors, s->transfers,
+		s->transfers ? (100 * s->errors / s->transfers) : 0);
+	s = &mmc->wr_stats;
+	printf("write %s. error %d/%d (%d%%)\n",
+	       sprintf_speed(buf,
+			     s->total_sz *  mmc->read_bl_len,
+			     s->total_time),
+		s->errors, s->transfers,
+		s->transfers ? (100 * s->errors / s->transfers) : 0);
+}
+
 static void print_mmcinfo(struct mmc *mmc)
 {
 	int i;
@@ -81,6 +117,7 @@ static void print_mmcinfo(struct mmc *mmc)
 			}
 		}
 	}
+	show_stats(mmc);
 }
 static struct mmc *init_mmc_device(int dev, bool force_init)
 {

commit b78c66403e5024984e26965ad7ac78cfe19949e2

cmd: mmc: fix division by zero
Fix division by zero in sprintf_speed() when not even
a single mmc write has happened, and "mmc info" is
invoked.

This causes division-by-zero error because total_time
in write statistic is still 0.

Fixes: 7a0a68ac5b5b ("cmd: mmc: make command "mmc info" show the statistics")
Signed-off-by: Sekhar Nori <nsekhar@ti.com>

cmd/mmc.c
@@ -15,15 +15,28 @@ static int curr_device = -1;
 
 static char *sprintf_speed(char *buf, ulong sz, uint ms)
 {
-	uint64_t kbytes_per_sec = ((sz >> 10) * 1000) / ms;
-	uint64_t mb_per_sec = kbytes_per_sec >> 10;
-	uint32_t remain = ((kbytes_per_sec - (mb_per_sec << 10)) * 100) >> 10;
-	if (ms && mb_per_sec)
+	uint64_t kbytes_per_sec;
+	uint64_t mb_per_sec;
+	uint32_t remain;
+
+	if (!ms)
+		goto out;
+
+	kbytes_per_sec = ((sz >> 10) * 1000) / ms;
+	mb_per_sec = kbytes_per_sec >> 10;
+	remain = ((kbytes_per_sec - (mb_per_sec << 10)) * 100) >> 10;
+
+	if (mb_per_sec)
 		sprintf(buf, "%llu.%02u MB/s", mb_per_sec, remain);
-	else if (ms && kbytes_per_sec)
+	else if (kbytes_per_sec)
 		sprintf(buf, "%llu KB/s", kbytes_per_sec);
 	else
-		strcpy(buf, "### MB/s");
+		goto out;
+
+	return buf;
+
+out:
+	strcpy(buf, "### MB/s");
 	return buf;
 }
 