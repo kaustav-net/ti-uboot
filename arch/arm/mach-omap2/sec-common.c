kaustav-net/ti-uboot

commit 266b5c0cdbd1150cf5e6bde0d473e5d2f0f60812

arm: am33xx: security: adds auth support for encrypted images
This patch adds support for authentication of both plain
text and encrypted binaries. A new SECDEV package is needed
to enable encryption of binaries by default for AM3x.

The ROM authentication API detects encrypted images at
runtime and automatically decrypts the image if the
signature verification passes.

Addition of encryption on AM3x results in a change in the
image format. On AM4x, AM5x and, on AM3x devices signing
clear test images, the signature is appended to the end of the
binary.

On AM3x, when the SECDEV package is used to create signed
and encrypted images, the signature is added as a header
to the start of the binary. So the binary size calculation
has been updated to reflect this change.

The signing tools and encrypted image format for AM3x
cannot be changed to behave like AM4x and AM5x to
maintain backward compatibility with older Sitara
M-Shield releases.

Adding encryption support also increases the size of
the PPA. As the SPL is loaded right after the PPA for
any peripheral boot, this increase in PPA size results
in the SPL load address moving by 0x200 bytes (for UART boot).
Memory boot modes like MMC are not affected, as the ROM
loads the PPA and SPL in two separate steps.

Acked-by: Andrew F. Davis <afd@ti.com>
Signed-off-by: Madan Srinivas <madans@ti.com>

arch/arm/mach-omap2/sec-common.c
@@ -41,6 +41,9 @@
 #define PPA_SERV_HAL_SETUP_EMIF_FW_REGION   (PPA_HAL_SERVICES_START_INDEX + 26)
 #define PPA_SERV_HAL_LOCK_EMIF_FW           (PPA_HAL_SERVICES_START_INDEX + 27)
 
+/* Offset of header size if image is signed as ISW */
+#define HEADER_SIZE_OFFSET	(0x6D)
+
 int tee_loaded = 0;
 
 /* Argument for PPA_SERV_HAL_TEE_LOAD_MASTER */
@@ -123,6 +126,9 @@ int secure_boot_verify_image(void **image, size_t *size)
 	}
 
 	*size = sig_addr - cert_addr;	/* Subtract out the signature size */
+	/* Subtract header if present */
+	if (strncmp((char *)sig_addr, "CERT_ISW_", 9) == 0)
+		*size = ((u32 *)*image)[HEADER_SIZE_OFFSET];
 	cert_size = *size;
 
 	/* Check if image load address is 32-bit aligned */

configs/am335x_hs_evm_uart_defconfig
@@ -8,7 +8,7 @@ CONFIG_AM33XX=y
 # CONFIG_SPL_NAND_SUPPORT is not set
 CONFIG_TI_SECURE_DEVICE=y
 CONFIG_TARGET_AM335X_EVM=y
-CONFIG_ISW_ENTRY_ADDR=0x40301750
+CONFIG_ISW_ENTRY_ADDR=0x40301950
 CONFIG_SPL_STACK_R_ADDR=0x82000000
 CONFIG_DEFAULT_DEVICE_TREE="am335x-evm"
 CONFIG_DISTRO_DEFAULTS=y

commit 590c7d7fe15d06a4d708403d3234bcd01cd039e1

arm: am33xx: security: Fix size calculation on header
Fix the size calculation in the verify boot.  The original
patch 266b5c0cdbd1150cf5e6bde0d473e5d2f0f60812 was merged
but this was not the correct version.  The correct version
subtracted the header.

Fixes: 266b5c0cdbd1 ("arm: am33xx: security: adds auth support for encrypted images")
Signed-off-by: Madan Srinivas <madans@ti.com>
Signed-off-by: Dan Murphy <dmurphy@ti.com>

arch/arm/mach-omap2/sec-common.c
@@ -128,7 +128,7 @@ int secure_boot_verify_image(void **image, size_t *size)
 	*size = sig_addr - cert_addr;	/* Subtract out the signature size */
 	/* Subtract header if present */
 	if (strncmp((char *)sig_addr, "CERT_ISW_", 9) == 0)
-		*size = ((u32 *)*image)[HEADER_SIZE_OFFSET];
+		*size -= ((u32 *)*image)[HEADER_SIZE_OFFSET];
 	cert_size = *size;
 
 	/* Check if image load address is 32-bit aligned */