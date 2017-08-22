8e68971f508bc3ab70202d7eed614de4f7ac5d22
Lokesh Vutla <lokeshvutla@ti.com>
2017-08-22 12:08:50 +0530

    configs: ti_omap5_common: Select dtb name for x15 revC
    Select dtb name for x15 revC.
    
    Tested-By: Praneeth Bajjuri <praneeth@ti.com>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- include/configs/ti_omap5_common.h
+++ include/configs/ti_omap5_common.h
@@ -95,6 +95,8 @@
 			"setenv fdtfile am57xx-beagle-x15.dtb; fi;" \
 		"if test $board_name = beagle_x15_revb1; then " \
 			"setenv fdtfile am57xx-beagle-x15-revb1.dtb; fi;" \
+		"if test $board_name = beagle_x15_revc; then " \
+			"setenv fdtfile am57xx-beagle-x15-revc.dtb; fi;" \
 		"if test $board_name = am572x_idk && test $idk_lcd = no; then " \
 			"setenv fdtfile am572x-idk.dtb; fi;" \
 		"if test $board_name = am572x_idk && test $idk_lcd = osd101t2045; then " \
