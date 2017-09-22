b8ed28f9d8d3dfb0b73466b3d8b3d1a92447d87d
Lokesh Vutla <lokeshvutla@ti.com>
2017-09-22 16:19:31 +0530

    board: ti: dra7xx: Fix iodelay execution sequence
    commit 2f2928878591 ("board: ti: dra7xx: Fix iodelay execution sequence") branch ti-u-boot-2017.01
    
    commit c3ca2d1a40877 ("board: ti: dra7xx: Select MCAN instead
    of DCAN on DRA76 EVM") selects MCAN pinmux. As part of this commit,
    iodelay_start() sequence is moved before pads selection. Once this
    start sequence is called, SoC will be in isolated state. On DRA71 EVM
    NAND pins are selected based on an i2c read, which will fail
    as SoC is in isolation. Also subsequent i2c reads/writes on the same
    bus are timedout(failing avs programming). Fix it by calling
    the iodelay_start() sequence just before configuring pinmux.
    
    Acked-by: Nishanth Menon <nm@ti.com>
    Tested-by: Tero Kristo <t-kristo@ti.com>
    Reviewed-by: Franklin S Cooper Jr. <fcooper@ti.com>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

--- board/ti/dra7xx/evm.c
+++ board/ti/dra7xx/evm.c
@@ -751,11 +751,6 @@ void recalibrate_iodelay(void)
 	int npads, niodelays, delta_npads = 0;
 	int ret;
 
-	/* Setup I/O isolation */
-	ret = __recalibrate_iodelay_start();
-	if (ret)
-		goto err;
-
 	switch (omap_revision()) {
 	case DRA722_ES1_0:
 	case DRA722_ES2_0:
@@ -793,9 +788,6 @@ void recalibrate_iodelay(void)
 		npads = ARRAY_SIZE(dra76x_core_padconf_array);
 		iodelay = dra76x_es1_0_iodelay_cfg_array;
 		niodelays = ARRAY_SIZE(dra76x_es1_0_iodelay_cfg_array);
-		/* Set mux for MCAN instead of DCAN1 */
-		clrsetbits_le32((*ctrl)->control_core_control_spare_rw,
-				MCAN_SEL_ALT_MASK, MCAN_SEL);
 		break;
 	default:
 	case DRA752_ES2_0:
@@ -809,6 +801,11 @@ void recalibrate_iodelay(void)
 		break;
 	}
 
+	/* Setup I/O isolation */
+	ret = __recalibrate_iodelay_start();
+	if (ret)
+		goto err;
+
 	/* Do the muxing here */
 	do_set_mux32((*ctrl)->control_padconf_core_base, pads, npads);
 
@@ -816,6 +813,11 @@ void recalibrate_iodelay(void)
 	if (delta_npads)
 		do_set_mux32((*ctrl)->control_padconf_core_base,
 			     delta_pads, delta_npads);
+
+	if (is_dra76x())
+		/* Set mux for MCAN instead of DCAN1 */
+		clrsetbits_le32((*ctrl)->control_core_control_spare_rw,
+				MCAN_SEL_ALT_MASK, MCAN_SEL);
 
 	/* Setup IOdelay configuration */
 	ret = do_set_iodelay((*ctrl)->iodelay_config_base, iodelay, niodelays);
