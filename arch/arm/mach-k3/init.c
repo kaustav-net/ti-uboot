67077a4fc67930b82b9715aae9ab403d252a64a2
Lokesh Vutla <lokeshvutla@ti.com>
2017-11-10 19:23:19 +0530

    armv7R: K3: am654: Enable MPU regions
    Enable MPU regions for AM654 evm:
    - Region0: 0x00000000 - 0xFFFFFFFF: Device memory, not executable
    - Region1: 0x41c00000 - 0x42400000: Normal, executable, WB, Write alloc
    - Region2: 0x80000000 - 0xFFFFFFFF: Normal, executable, WB, Write alloc
    - region3-15: Disabled
    
    With this dcache can be enabled either in SPL or U-Boot.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- arch/arm/mach-k3/init.c
+++ arch/arm/mach-k3/init.c
@@ -11,8 +11,41 @@
 #include <asm/io.h>
 #include <spl.h>
 #include <asm/arch/hardware.h>
+#include <asm/armv7_mpu.h>
 
 #ifdef CONFIG_SPL_BUILD
+#ifdef CONFIG_CPU_V7R
+struct mpu_region_config k3_mpu_regions[16] = {
+	/*
+	 * Make all 4GB as Device Memory and not executable. We are overriding
+	 * it with next region for any requirement.
+	 */
+	{0x00000000, REGION_0, XN_EN, PRIV_RW_USR_RW, SHARED_WRITE_BUFFERED,
+	 REGION_4GB},
+
+	/* SPL code area marking it as WB and Write allocate. */
+	{CONFIG_SPL_TEXT_BASE, REGION_1, XN_DIS, PRIV_RW_USR_RW,
+	 O_I_WB_RD_WR_ALLOC, REGION_8MB},
+
+	/* U-Boot's code area marking it as WB and Write allocate */
+	{CONFIG_SYS_SDRAM_BASE, REGION_2, XN_DIS, PRIV_RW_USR_RW,
+	 O_I_WB_RD_WR_ALLOC, REGION_2GB},
+	{0x0, 3, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 4, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 5, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 6, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 7, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 8, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 9, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 10, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 11, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 12, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 13, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 14, 0x0, 0x0, 0x0, 0x0},
+	{0x0, 15, 0x0, 0x0, 0x0, 0x0},
+};
+#endif
+
 static void mmr_unlock(u32 base, u32 partition)
 {
 	/* Translate the base address */
@@ -66,6 +99,10 @@ void board_init_f(ulong dummy)
 
 	/* Make all control module registers accessible */
 	ctrl_mmr_unlock();
+
+#ifdef CONFIG_CPU_V7R
+	setup_mpu_regions(k3_mpu_regions, ARRAY_SIZE(k3_mpu_regions));
+#endif
 
 	/* Init DM early in-order to invoke system controller */
 	spl_early_init();
