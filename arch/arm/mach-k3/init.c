ca6f7651b0dbbf646443a3034cb6c5944ccfc48f
Lokesh Vutla <lokeshvutla@ti.com>
2017-11-12 00:29:13 +0530

    armv7R: K3: am654: Add support for loading firmware from FIT
    System Controller firmware is part of initial bootloader
    image that is loaded by ROM. A message with firmware location
    needs to be send to ROM in-order to load it. Add support
    for extracting firmware location from SPL_MULTI_DTB fit image
    and send a message to ROM.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- arch/arm/mach-k3/init.c
+++ arch/arm/mach-k3/init.c
@@ -10,8 +10,12 @@
 #include <common.h>
 #include <asm/io.h>
 #include <spl.h>
-#include <asm/arch/hardware.h>
+#include <remoteproc.h>
+#include <linux/libfdt.h>
+#include <image.h>
+#include <asm/sections.h>
 #include <asm/armv7_mpu.h>
+#include <asm/arch/hardware.h>
 
 #ifdef CONFIG_SPL_BUILD
 #ifdef CONFIG_CPU_V7R
@@ -89,8 +93,53 @@ static void store_boot_index_from_rom(void)
 	*boot_index = *(u32 *)(CONFIG_BOOT_PARAM_TABLE_INDEX);
 }
 
+#ifdef CONFIG_K3_LOAD_SYSFW
+static int locate_system_controller_firmware(int *addr, int *len)
+{
+	struct image_header *header;
+	int images, node;
+	const void *fit;
+
+	if (IS_ENABLED(CONFIG_SPL_SEPARATE_BSS))
+		fit = (ulong *)&_image_binary_end;
+	else
+		fit = (ulong *)&__bss_end;
+
+	header = (struct image_header *)fit;
+	if (image_get_magic(header) != FDT_MAGIC) {
+		debug("No FIT image appended to SPL\n");
+		return -EINVAL;
+	}
+
+	/* find the node holding the images information */
+	images = fdt_path_offset(fit, FIT_IMAGES_PATH);
+	if (images < 0) {
+		debug("%s: Cannot find /images node: %d\n", __func__, images);
+		return -ENOENT;
+	}
+
+	/* Find the subnode holding system controller firmware */
+	node = fdt_subnode_offset(fit, images, "sysfw");
+	if (node < 0) {
+		debug("%s: Cannot find fdt node sysfw in FIT: %d\n",
+		      __func__, node);
+		return -EINVAL;
+	}
+
+	fit_image_get_data_offset(fit, node, addr);
+	*addr += (int)fit + ((fdt_totalsize(fit) + 3) & ~3);
+	fit_image_get_data_size(fit, node, len);
+
+	return 0;
+}
+#endif
+
 void board_init_f(ulong dummy)
 {
+#ifdef CONFIG_K3_LOAD_SYSFW
+	int ret, fw_addr, len;
+#endif
+
 	/*
 	 * Cannot delay this further as there is a chance that
 	 * BOOT_PARAM_TABLE_INDEX can be over written by SPL MALLOC section.
@@ -106,6 +155,38 @@ void board_init_f(ulong dummy)
 
 	/* Init DM early in-order to invoke system controller */
 	spl_early_init();
+
+#ifdef CONFIG_K3_LOAD_SYSFW
+	/* Try to locate firmware image and load it to system controller */
+	if (!locate_system_controller_firmware(&fw_addr, &len)) {
+		debug("Firmware located. Now try to load\n");
+		/*
+		 * It is assumed that remoteproc device 0 is the corresponding
+		 * system-controller that runs SYSFW.
+		 * Make sure DT reflects the same.
+		 */
+		ret = rproc_dev_init(0);
+		if (ret) {
+			debug("rproc failed to be initialized: ret= %d\n",
+			      ret);
+			return;
+		}
+
+		ret = rproc_load(0, fw_addr, len);
+		if (ret) {
+			debug("Firmware failed to start on rproc: ret= %d\n",
+			      ret);
+			return;
+		}
+
+		ret = rproc_start(0);
+		if (ret) {
+			debug("Firmware init failed on rproc: ret= %d\n",
+			      ret);
+			return;
+		}
+	}
+#endif
 
 	/* Prepare console output */
 	preloader_console_init();
