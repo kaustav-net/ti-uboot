40ac04ce46804c0dd983624f5258690e744d9b65
Lokesh Vutla <lokeshvutla@ti.com>
2017-11-17 10:27:13 +0530

    arm: K3: am654: Add support for boot device detection
    AM654 allows for booting from primary or backup boot media.
    Both media can be chosen individually based on switch settings.
    ROM looks for a valid image in primary boot media, if not found
    then looks in backup boot media. In order to pass this boot media
    information to boot loader, ROM stores a value at a particular
    address. Add support for reading this information and determining
    the boot media correctly.
    
    BOOT_DEVICE_SPI is generic bootdevice name for SPI/OSPI/QSPI in SPL.
    Hence, map OSPI/QSPI boot mode to BOOT_DEVICE_SPI.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    Signed-off-by: Andreas Dannenberg <dannenberg@ti.com>
    Signed-off-by: Vignesh R <vigneshr@ti.com>

---
+++ arch/arm/mach-k3/include/mach/hardware.h
@@ -0,0 +1,24 @@
+/*
+ * K3: Common SoC definitions, structures etc.
+ *
+ * (C) Copyright (C) 2018 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+#ifndef __ASM_ARCH_HARDWARE_H
+#define __ASM_ARCH_HARDWARE_H
+
+#include <config.h>
+
+#define CTRL_MMR0_BASE					0x00100000
+#define CTRLMMR_MAIN_DEVSTAT				(CTRL_MMR0_BASE + 0x30)
+
+#define CTRLMMR_MAIN_DEVSTAT_BOOTMODE_MASK		GENMASK(3, 0)
+#define CTRLMMR_MAIN_DEVSTAT_BOOTMODE_SHIFT		0
+#define CTRLMMR_MAIN_DEVSTAT_BKUP_BOOTMODE_MASK		GENMASK(6, 4)
+#define CTRLMMR_MAIN_DEVSTAT_BKUP_BOOTMODE_SHIFT	4
+
+/* MCU SCRATCHPAD usage */
+#define K3_BOOT_PARAM_TABLE_INDEX_VAL	CONFIG_MCU_SCRATCHPAD_BASE
+
+#endif /* __ASM_ARCH_HARDWARE_H */
--- arch/arm/mach-k3/init.c
+++ arch/arm/mach-k3/init.c
@@ -8,11 +8,26 @@
  */
 
 #include <common.h>
+#include <asm/io.h>
 #include <spl.h>
+#include <asm/arch/hardware.h>
 
 #ifdef CONFIG_SPL_BUILD
+static void store_boot_index_from_rom(void)
+{
+	u32 *boot_index = (u32 *)K3_BOOT_PARAM_TABLE_INDEX_VAL;
+
+	*boot_index = *(u32 *)(CONFIG_BOOT_PARAM_TABLE_INDEX);
+}
+
 void board_init_f(ulong dummy)
 {
+	/*
+	 * Cannot delay this further as there is a chance that
+	 * BOOT_PARAM_TABLE_INDEX can be over written by SPL MALLOC section.
+	 */
+	store_boot_index_from_rom();
+
 	/* Init DM early in-order to invoke system controller */
 	spl_early_init();
 
@@ -20,9 +35,46 @@ void board_init_f(ulong dummy)
 	preloader_console_init();
 }
 
+static u32 __get_backup_bootmedia(u32 devstat)
+{
+	u32 bkup_boot = (devstat & CTRLMMR_MAIN_DEVSTAT_BKUP_BOOTMODE_MASK) >>
+			CTRLMMR_MAIN_DEVSTAT_BKUP_BOOTMODE_SHIFT;
+
+	switch (bkup_boot) {
+#define __BKUP_BOOT_DEVICE(n)			\
+	case BACKUP_BOOT_DEVICE_##n:		\
+		return BOOT_DEVICE_##n;
+	__BKUP_BOOT_DEVICE(USB);
+	__BKUP_BOOT_DEVICE(UART);
+	__BKUP_BOOT_DEVICE(ETHERNET);
+	__BKUP_BOOT_DEVICE(MMC2);
+	__BKUP_BOOT_DEVICE(SPI);
+	__BKUP_BOOT_DEVICE(HYPERFLASH);
+	__BKUP_BOOT_DEVICE(I2C);
+	};
+
+	return BOOT_DEVICE_RAM;
+}
+
+static u32 __get_primary_bootmedia(u32 devstat)
+{
+	u32 bootmode = devstat & CTRLMMR_MAIN_DEVSTAT_BOOTMODE_MASK;
+
+	if (bootmode == BOOT_DEVICE_OSPI || bootmode ==	BOOT_DEVICE_QSPI)
+		bootmode = BOOT_DEVICE_SPI;
+
+	return bootmode;
+}
+
 u32 spl_boot_device(void)
 {
-	return BOOT_DEVICE_RAM;
+	u32 devstat = readl(CTRLMMR_MAIN_DEVSTAT);
+	u32 bootindex = readl(K3_BOOT_PARAM_TABLE_INDEX_VAL);
+
+	if (bootindex == K3_PRIMARY_BOOTMODE)
+		return __get_primary_bootmedia(devstat);
+	else
+		return __get_backup_bootmedia(devstat);
 }
 #endif
 
