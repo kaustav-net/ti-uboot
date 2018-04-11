8ab21b18de042ee1a71efc6c95a9fec8bf0626e3
Jean-Jacques Hiblot <jjhiblot@ti.com>
2018-04-11 10:44:54 +0200

    board: ti: dra71-lcard: mux_data: Mux RGMII0_RXC for the GPIO function (GPIO5_26)
    This GPIO is used by linux to drive a mux selecting either the wifi or the
    sdcard on MMC1.
    
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

--- board/ti/dra7xx/mux_data.h
+++ board/ti/dra7xx/mux_data.h
@@ -549,6 +549,7 @@ const struct pad_conf_entry dra71x_lcard_core_padconf_array[] = {
 	{WAKEUP3, (M1 | PIN_OUTPUT)},	/* N/A.sys_nirq1 */
 	{MCASP1_ACLKR, (M10 | PIN_INPUT_PULLUP)},	/* mcasp1_aclkr.i2c4_sda */
 	{MCASP1_FSR, (M10 | PIN_INPUT_PULLUP)},	/* mcasp1_fsr.i2c4_scl */
+	{RGMII0_RXC, (M14 | PIN_OUTPUT)},	/* rgmii0_rxc.gpio5_26 */
 };
 
 const struct pad_conf_entry early_padconf[] = {
