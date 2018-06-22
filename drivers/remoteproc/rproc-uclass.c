kaustav-net/ti-uboot

commit ea635413ae1b23b11270f4501e0ce75e3731113c

remoteproc: Introduce support for processor control
Modern System on Chip solutions are coming up with very
complex architectures and with multiple processing entities
on the same die, each entity being dedicated for its own specific
functionality.

In certain classes of products where safety is the major concern,
the controlling of other processing entities cannot be done directly
by the one running u-boot or HLOS. All the controlling capabilities
are abstracted and done by a centralized processor acting as master
of the SoC. This centralized processor has a complete picture on the
current status of the SoC. Typical example would be of TI's K3 generation
SoCs where SYSFW(M3) acts as a centralized master. For u-boot to load an
image to any other core, it should communicate with SYSFW.

In order to address these needs, a wrapper layer should exist
between remoreproc driver and the communication protocol to the
master core. This patch adds a wrapper layer using a new UCLASS_PROC_CONTROL
driver. All the services that are provided by the remoteproc driver
are available by this wrapper layer, so that any remoteproc driver
uses these services as necessary.

Sandbox tests to be added?

Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

drivers/remoteproc/rproc-uclass.c
@@ -8,6 +8,7 @@
 #include <errno.h>
 #include <fdtdec.h>
 #include <malloc.h>
+#include <dm/ofnode.h>
 #include <remoteproc.h>
 #include <asm/io.h>
 #include <dm/device-internal.h>
@@ -433,4 +434,116 @@ int rproc_ping(int id)
 int rproc_is_running(int id)
 {
 	return _rproc_ops_wrapper(id, RPROC_RUNNING);
+};
+
+/*------------------------------------------------------------------------*/
+/* Processor Control operations						  */
+/*------------------------------------------------------------------------*/
+static inline struct proc_ctrl_ops *proc_ctrl_dev_ops(struct udevice *dev)
+{
+	return (struct proc_ctrl_ops *)dev->driver->ops;
+}
+
+static int proc_ctrl_of_xlate_default(struct proc_ctrl *ctrl,
+				      struct ofnode_phandle_args *args)
+{
+	debug("%s(ctrl=%p)\n", __func__, ctrl);
+
+	if (args->args_count > 1) {
+		debug("Invalid args_count: %d\n", args->args_count);
+		return -EINVAL;
+	}
+
+	if (args->args_count)
+		ctrl->id = args->args[0];
+	else
+		ctrl->id = 0;
+
+	return 0;
+}
+
+static int proc_ctrl_get_by_indexed_prop(struct udevice *rproc_dev,
+					 const char *prop_name, int index,
+					 struct proc_ctrl *ctrl)
+{
+	struct ofnode_phandle_args args;
+	const struct proc_ctrl_ops *ops;
+	struct udevice *dev;
+	int ret;
+
+	debug("%s(rproc_dev=%p, index=%d, ctrl=%p)\n", __func__,
+	      rproc_dev, index, ctrl);
+
+	assert(ctrl);
+	ctrl->dev = NULL;
+
+	ret = dev_read_phandle_with_args(rproc_dev, prop_name, "#proc-cells",
+					 0, index, &args);
+	if (ret) {
+		debug("%s: dev_read_phandle_with_args failed: err=%d\n",
+		      __func__, ret);
+		return ret;
+	}
+
+	ret = uclass_get_device_by_ofnode(UCLASS_PROC_CONTROL, args.node, &dev);
+	if (ret) {
+		debug("%s: uclass_get_device_by_ofnode failed: err=%d\n",
+		      __func__, ret);
+		return ret;
+	}
+
+	ctrl->dev = dev;
+
+	ops = proc_ctrl_dev_ops(dev);
+
+	if (ops->of_xlate)
+		ret = ops->of_xlate(ctrl, &args);
+	else
+		ret = proc_ctrl_of_xlate_default(ctrl, &args);
+
+	if (ret) {
+		debug("of_xlate() failed: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+int proc_ctrl_get_by_index(struct udevice *dev, int index,
+			   struct proc_ctrl *proc)
+{
+	return proc_ctrl_get_by_indexed_prop(dev, "proc-controls", index, proc);
+}
+
+int proc_ctrl_rproc_load(struct proc_ctrl *ctrl, ulong addr, ulong size)
+{
+	struct proc_ctrl_ops *ops = proc_ctrl_dev_ops(ctrl->dev);
+
+	debug("%s(proc_ctrl=%p)\n", __func__, ctrl);
+
+	return ops->load(ctrl, addr, size);
+}
+
+#define PROC_CTRL_API(fn)						\
+int proc_ctrl_rproc_##fn(struct proc_ctrl *ctrl)			\
+{									\
+	struct proc_ctrl_ops *ops = proc_ctrl_dev_ops(ctrl->dev);	\
+									\
+	debug("%s(proc_ctrl=%p)\n", __func__, ctrl);			\
+									\
+	return ops->fn(ctrl);						\
+}
+
+PROC_CTRL_API(request)
+PROC_CTRL_API(release)
+PROC_CTRL_API(init)
+PROC_CTRL_API(start)
+PROC_CTRL_API(stop)
+PROC_CTRL_API(reset)
+PROC_CTRL_API(is_running)
+PROC_CTRL_API(ping)
+
+UCLASS_DRIVER(proccontrol) = {
+	.id = UCLASS_PROC_CONTROL,
+	.name = "procctrl",
 };

include/remoteproc.h
@@ -168,4 +168,74 @@ static inline int rproc_ping(int id) { return -ENOSYS; }
 static inline int rproc_is_running(int id) { return -ENOSYS; }
 #endif
 
+/**
+ * struct proc_ctrl - A handle to Remote Processor control data
+ * @dev:	Processor control device pointer
+ * @id:		ID of the remote processor ID as
+ *		understood by the Processor control
+ * @data:	Private data of the remote processor (Optional)
+ *
+ * @id and @data is updated by the xlate api. This information is used by
+ * other services provided by the processor control.
+ */
+struct proc_ctrl {
+	struct udevice *dev;
+	unsigned long id;
+	unsigned long data;
+};
+
+/**
+ * struct proc_ctrl_ops - Operations that are provided by the processor control
+ *				driver.
+ * @request:	Request for controlling the remote core.
+ * @release:	Release the access to the remote core.
+ * @init:	Initialize the remoteproc device.
+ * @load:	Load the remoteproc device using data provided(mandatory)
+ *		This takes the following additional arguments.
+ *			addr- Address of the binary image to be loaded
+ *			size- Size of the binary image to be loaded
+ * @start:	Start the remoteproc device
+ * @stop:	Stop the remoteproc device
+ * @reset:	Reset the remote proc device
+ * @is_running:	Check if the remote processor is running
+ * @ping:	Ping the remote device for basic communication check
+ * @of_xlate:	Convert the DT information to proc_ctrl structure.
+ */
+struct proc_ctrl_ops {
+	int (*request)(struct proc_ctrl *ctrl);
+	int (*release)(struct proc_ctrl *ctrl);
+	int (*init)(struct proc_ctrl *ctrl);
+	int (*load)(struct proc_ctrl *ctrl, ulong addr, ulong size);
+	int (*start)(struct proc_ctrl *ctrl);
+	int (*stop)(struct proc_ctrl *ctrl);
+	int (*reset)(struct proc_ctrl *ctrl);
+	int (*is_running)(struct proc_ctrl *ctrl);
+	int (*ping)(struct proc_ctrl *ctrl);
+	int (*of_xlate)(struct proc_ctrl *ctrl,
+			struct ofnode_phandle_args *args);
+};
+
+int proc_ctrl_get_by_index(struct udevice *dev, int index,
+			   struct proc_ctrl *proc);
+/**
+ * Processor control apis that act as wrapper between the remoteproc driver
+ * and the processing entity that can control the remote core. All the services
+ * that are provided by the remoteproc driver are support here, additionally
+ * there are two other apis that are supported:
+ * proc_ctrl_rproc_request() -  API to request access for controlling the
+ *				remote processor.
+ * proc_ctrl_rproc_release() - API to release access for controlling the
+ *				remote core.
+ * These apis should be called by the remoteproc driver in their respective
+ * service calls.
+ */
+int proc_ctrl_rproc_request(struct proc_ctrl *ctrl);
+int proc_ctrl_rproc_release(struct proc_ctrl *ctrl);
+int proc_ctrl_rproc_init(struct proc_ctrl *ctrl);
+int proc_ctrl_rproc_load(struct proc_ctrl *ctrl, ulong addr, ulong size);
+int proc_ctrl_rproc_start(struct proc_ctrl *ctrl);
+int proc_ctrl_rproc_stop(struct proc_ctrl *ctrl);
+int proc_ctrl_rproc_reset(struct proc_ctrl *ctrl);
+int proc_ctrl_rproc_is_running(struct proc_ctrl *ctrl);
+int proc_ctrl_rproc_ping(struct proc_ctrl *ctrl);
 #endif	/* _RPROC_H_ */

include/dm/uclass-id.h
@@ -65,6 +65,7 @@ enum uclass_id {
 	UCLASS_PMIC,		/* PMIC I/O device */
 	UCLASS_PWM,		/* Pulse-width modulator */
 	UCLASS_POWER_DOMAIN,	/* (SoC) Power domains */
+	UCLASS_PROC_CONTROL,	/* Processor control device */
 	UCLASS_PWRSEQ,		/* Power sequence device */
 	UCLASS_RAM,		/* RAM controller */
 	UCLASS_REGULATOR,	/* Regulator device */

commit d12f96fa85605512e9907eeb436cd086c07b9fee

remoteproc: Include dm/ofnode.h inside remoteproc.h
proc_control_ops uses structure ofnode_phandle_args which is
defined under dm/ofnode.h. Include this file inside remoteproc.h
to avoid any dependency.
Without this if rproc-uclass is not used, the following build warning
appears:

In file included from board/ti/ks2_evm/board_k2g.c:16:0:
include/remoteproc.h:215:11: warning: ‘struct ofnode_phandle_args’ declared inside parameter list will not be visible outside of this definition or declaration
    struct ofnode_phandle_args *args);

Fixes: ea635413ae1b ("remoteproc: Introduce support for processor control")
Reported-by: Vignesh R <vigneshr@ti.com>
Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

drivers/remoteproc/rproc-uclass.c
@@ -8,7 +8,6 @@
 #include <errno.h>
 #include <fdtdec.h>
 #include <malloc.h>
-#include <dm/ofnode.h>
 #include <remoteproc.h>
 #include <asm/io.h>
 #include <dm/device-internal.h>

include/remoteproc.h
@@ -14,6 +14,7 @@
  * platforms have moved to dm/fdt.
  */
 #include <dm/platdata.h>	/* For platform data support - non dt world */
+#include <dm/ofnode.h>
 
 /**
  * enum rproc_mem_type - What type of memory model does the rproc use