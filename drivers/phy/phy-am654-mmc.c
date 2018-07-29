0e1ddbbaa6bebe40e1e1715c1a038c85d0fd6369
Faiz Abbas <faiz_abbas@ti.com>
2018-07-29 15:15:50 +0530

    phy: phy-am654-mmc: Add Support for phy_power_on and phy_power_off
    Switching the clock speed requires a power_off and power_on of the
    mmc phy. Add support for the same.
    
    Signed-off-by: Faiz Abbas <faiz_abbas@ti.com>

--- drivers/phy/phy-am654-mmc.c
+++ drivers/phy/phy-am654-mmc.c
@@ -29,13 +29,25 @@
 #define DLLRDY_SHIFT		0
 #define DLLRDY_MASK		BIT(DLLRDY_SHIFT)
 
+#define DRIVER_STRENGTH_50_OHM	0x0
+#define DRIVER_STRENGTH_33_OHM	0x1
+#define DRIVER_STRENGTH_66_OHM	0x2
+#define DRIVER_STRENGTH_100_OHM	0x3
+#define DRIVER_STRENGTH_40_OHM	0x4
+
+#include <clk.h>
 #include <common.h>
 #include <dm.h>
 #include <generic-phy.h>
 #include <regmap.h>
 
+DECLARE_GLOBAL_DATA_PTR;
+
 struct am654_mmc_phy {
 	struct regmap *reg_base;
+	u32 otap_del_sel;
+	u32 trm_icp;
+	u32 drv_strength;
 };
 
 static int am654_mmc_phy_init(struct phy *phy)
@@ -49,13 +61,126 @@ static int am654_mmc_phy_init(struct phy *phy)
 	return 0;
 }
 
+static int am654_mmc_phy_power_on(struct phy *phy)
+{
+	struct am654_mmc_phy *mmc_phy = dev_get_priv(phy->dev);
+	u32 mask, val;
+	int sel50, sel100;
+	int rate;
+
+	if (!phy->priv) {
+		dev_err(phy->dev, "No clock speed\n");
+		return -EINVAL;
+	}
+
+	rate = *(unsigned int *)phy->priv;
+
+	/* Setup DLL Output TAP delay */
+	mask = OTAPDLYENA_MASK | OTAPDLYSEL_MASK;
+	val = (1 << OTAPDLYENA_SHIFT) |
+	      (mmc_phy->otap_del_sel << OTAPDLYSEL_SHIFT);
+	regmap_update_bits(mmc_phy->reg_base, PHYCTRL_CTRL4_REG,
+			   mask, val);
+
+	switch (rate) {
+	case 200000000:
+		sel50 = 0;
+		sel100 = 0;
+		break;
+	case 100000000:
+		sel50 = 0;
+		sel100 = 1;
+		break;
+	default:
+		sel50 = 1;
+		sel100 = 0;
+	}
+
+	/* Configure PHY DLL frequency */
+	mask = SEL50_MASK | SEL100_MASK;
+	val = (sel50 << SEL50_SHIFT) | (sel100 << SEL100_SHIFT);
+	regmap_update_bits(mmc_phy->reg_base, PHYCTRL_CTRL5_REG,
+			   mask, val);
+
+	/* Configure DLL TRIM */
+	mask = DLL_TRIM_ICP_MASK;
+	val = mmc_phy->trm_icp << DLL_TRIM_ICP_SHIFT;
+
+	/* Configure DLL driver strength */
+	mask |= DR_TY_MASK;
+	val |= mmc_phy->drv_strength << DR_TY_SHIFT;
+	regmap_update_bits(mmc_phy->reg_base, PHYCTRL_CTRL1_REG, mask, val);
+
+	/* Enable DLL */
+	regmap_update_bits(mmc_phy->reg_base, PHYCTRL_CTRL1_REG,
+			   ENDLL_MASK, 0x1 << ENDLL_SHIFT);
+
+	/*
+	 * Poll for DLL ready. Use a one second timeout.
+	 * Works in all experiments done so far
+	 */
+	return regmap_read_poll_timeout(mmc_phy->reg_base, PHYCTRL_STAT1_REG,
+					val, val & DLLRDY_MASK, 1000000);
+
+}
+
+static int am654_mmc_phy_power_off(struct phy *phy)
+{
+	struct am654_mmc_phy *mmc_phy = dev_get_priv(phy->dev);
+
+	/* Disable DLL */
+	regmap_update_bits(mmc_phy->reg_base, PHYCTRL_CTRL1_REG,
+			   ENDLL_MASK, 0);
+
+	return 0;
+}
+
 static const struct phy_ops am654_mmc_phy_ops = {
 	.init = am654_mmc_phy_init,
+	.power_on = am654_mmc_phy_power_on,
+	.power_off = am654_mmc_phy_power_off,
 };
 
 int am654_mmc_phy_probe(struct udevice *dev)
 {
 	struct am654_mmc_phy *mmc_phy = dev_get_priv(dev);
+	u32 drv_strength;
+	int err;
+
+	err = dev_read_u32(dev, "ti,otap-del-sel",
+			   &mmc_phy->otap_del_sel);
+	if (err)
+		return err;
+
+	err = dev_read_u32(dev, "ti,trm-icp",
+			   &mmc_phy->trm_icp);
+	if (err)
+		return err;
+
+	err = dev_read_u32(dev, "ti,drv-strength-ohm", &drv_strength);
+	if (err)
+		return err;
+
+	switch (drv_strength) {
+	case 50:
+		mmc_phy->drv_strength = DRIVER_STRENGTH_50_OHM;
+		break;
+	case 33:
+		mmc_phy->drv_strength = DRIVER_STRENGTH_33_OHM;
+		break;
+	case 66:
+		mmc_phy->drv_strength = DRIVER_STRENGTH_66_OHM;
+		break;
+	case 100:
+		mmc_phy->drv_strength = DRIVER_STRENGTH_100_OHM;
+		break;
+	case 40:
+		mmc_phy->drv_strength = DRIVER_STRENGTH_40_OHM;
+		break;
+	default:
+		dev_err(dev, "Invalid driver strength\n");
+		return -EINVAL;
+	}
 
 	return regmap_init_mem(dev, &mmc_phy->reg_base);
 }
