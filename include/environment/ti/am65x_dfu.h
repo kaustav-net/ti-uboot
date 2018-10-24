421ef73235cd1f99e2d17e584151f5332791320e
Faiz Abbas <faiz_abbas@ti.com>
2018-10-24 17:21:07 +0530

    configs: am65x_evm: Update dfu addresses for eMMC and OSPI
    The eMMC and OSPI image maps have been modified. Update the addresses
    in the dfu commands to reflect this change.
    
    Signed-off-by: Faiz Abbas <faiz_abbas@ti.com>

--- include/environment/ti/am65x_dfu.h
+++ include/environment/ti/am65x_dfu.h
@@ -21,21 +21,21 @@
 
 #define DFU_ALT_INFO_EMMC \
 	"dfu_alt_info_emmc=" \
-	"rawemmc raw 0 0x1000000 mmcpart 1;" \
+	"rawemmc raw 0 0x800000 mmcpart 1;" \
 	"rootfs part 0 1 mmcpart 0;" \
 	"tiboot3.bin.raw raw 0x0 0x400 mmcpart 1;" \
 	"tispl.bin.raw raw 0x400 0x1000 mmcpart 1;" \
-	"u-boot.img.raw raw 0x1400 0x2800 mmcpart 1;" \
-	"u-env.raw raw 0x3c00 0x100 mmcpart 1;" \
-	"sysfw.itb.raw raw 0x3d00 0x300 mmcpart 1\0"
+	"u-boot.img.raw raw 0x1400 0x2000 mmcpart 1;" \
+	"u-env.raw raw 0x3400 0x100 mmcpart 1;" \
+	"sysfw.itb.raw raw 0x3600 0x800 mmcpart 1\0"
 
 #define DFU_ALT_INFO_OSPI \
 	"dfu_alt_info_ospi=" \
 	"tiboot3.bin raw 0x0 0x080000;" \
 	"tispl.bin raw 0x080000 0x200000;" \
-	"u-boot.img raw 0x280000 0x500000;" \
-	"u-boot-env raw 0x780000 0x020000;" \
-	"sysfw.itb raw 0x7a0000 0x060000;" \
+	"u-boot.img raw 0x280000 0x400000;" \
+	"u-boot-env raw 0x680000 0x020000;" \
+	"sysfw.itb raw 0x6c0000 0x100000;" \
 	"rootfs raw 0x800000 0x3800000\0"
 
 #endif /* __TI_DFU_H */
