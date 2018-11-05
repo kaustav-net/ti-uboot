kaustav-net/ti-uboot

commit 5a8d506f21d24614811ba5f5e7456f377d260402

am65x_evm: fastboot: Enable reboot-bootloader
Implement fb_set_reboot_flag() function, which sets $dofastboot variable
whenever user request reboot-bootloader via fastboot. Check that
variable further to figure out if we need to run fastboot mode  right
away.

This patch is based on commit ecd8557937a8 ("fastboot: ARM: OMAP5: Enable
reboot-bootloader"), which implements the same functionality for OMAP5
platforms.

Acked-by: Praneeth Bajjuri <praneeth@ti.com>
Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

include/configs/am65x_evm.h
@@ -119,6 +119,15 @@
 
 /* Command for booting the Android from eMMC */
 #define EXTRA_ENV_AM65X_BOARD_SETTINGS_EMMC_ANDROID			\
+	"check_dofastboot="						\
+		"if test ${dofastboot} -eq 1; then "			\
+			"echo Boot fastboot requested, "		\
+				"resetting dofastboot ...;"		\
+			"setenv dofastboot 0; env save; "		\
+			"echo Booting into fastboot ...; "		\
+			"fastboot "					\
+			__stringify(CONFIG_FASTBOOT_USB_DEV) "; "	\
+		"fi\0"							\
 	"check_android="						\
 		"setenv mmcdev 0; "					\
 		"env delete boot_start; "				\

board/ti/am65x/evm.c
@@ -8,6 +8,7 @@
  */
 
 #include <common.h>
+#include <environment.h>
 #include <dm.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/hardware.h>
@@ -376,3 +377,13 @@ int board_late_init(void)
 
 	return 0;
 }
+
+#if defined(CONFIG_USB_FUNCTION_FASTBOOT) && !defined(CONFIG_ENV_IS_NOWHERE)
+int fb_set_reboot_flag(void)
+{
+	printf("Setting reboot to fastboot flag ...\n");
+	env_set("dofastboot", "1");
+	env_save();
+	return 0;
+}
+#endif

configs/am65x_evm_a53_defconfig
@@ -18,7 +18,7 @@ CONFIG_DEFAULT_DEVICE_TREE="k3-am654-base-board"
 CONFIG_DISTRO_DEFAULTS=y
 # CONFIG_SYS_MALLOC_CLEAR_ON_INIT is not set
 CONFIG_SPL_LOAD_FIT=y
-CONFIG_BOOTCOMMAND="run findfdt; run check_android; if test $is_android -eq 1; then run emmc_android_boot; fi; setenv mmcdev 1; run envboot; run init_${boot}; run get_kern_${boot}; run get_fdt_${boot}; run get_overlay_${boot}; run run_kern"
+CONFIG_BOOTCOMMAND="run check_dofastboot; run findfdt; run check_android; if test $is_android -eq 1; then run emmc_android_boot; fi; setenv mmcdev 1; run envboot; run init_${boot}; run get_kern_${boot}; run get_fdt_${boot}; run get_overlay_${boot}; run run_kern"
 # CONFIG_DISPLAY_CPUINFO is not set
 CONFIG_SPL_SYS_MALLOC_SIMPLE=y
 CONFIG_SPL_STACK_R=y

commit 9e1b118712213d2cab15280b5c6776bef35d4d59

am65x_evm: fastboot: Fix fastboot detection/trigger mechanism
The mechanism that checks if a fastboot should be performed by
testing the 'dofastboot' ENV variable for a value of 1 fails when
that ENV variable is not defined such as during a boot with the
initial/default environment, in which case the fastboot would
get triggered unintentionally.

Fix this by enclosing the dofastboot ENV variable in quotes so
that the test command evaluates as desired.

Fixes: 5a8d506f21d ("am65x_evm: fastboot: Enable reboot-bootloader")
Signed-off-by: Andreas Dannenberg <dannenberg@ti.com>
Acked-by: Praneeth Bajjuri <praneeth@ti.com>
Reviewed-by: Sam Protsenko <semen.protsenko@linaro.org>

include/configs/am65x_evm.h
@@ -120,7 +120,7 @@
 /* Command for booting the Android from eMMC */
 #define EXTRA_ENV_AM65X_BOARD_SETTINGS_EMMC_ANDROID			\
 	"check_dofastboot="						\
-		"if test ${dofastboot} -eq 1; then "			\
+		"if test \"${dofastboot}\" -eq 1; then "		\
 			"echo Boot fastboot requested, "		\
 				"resetting dofastboot ...;"		\
 			"setenv dofastboot 0; env save; "		\