kaustav-net/ti-uboot

commit 52635614056fd1e792d367dd04dc46c5cc8d3763

driver: net: ti: cpsw-mdio: use phys_addr_t for mdio_base addr
Use phys_addr_t for mdio_base address to avoid build warnings on arm64.

Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>
Signed-off-by: Keerthy <j-keerthy@ti.com>
Reviewed-by: Tom Rini <trini@konsulko.com>
Signed-off-by: Vignesh R <vigneshr@ti.com>

drivers/net/ti/cpsw_mdio.c
@@ -125,7 +125,7 @@ u32 cpsw_mdio_get_alive(struct mii_dev *bus)
 	return val & GENMASK(15, 0);
 }
 
-struct mii_dev *cpsw_mdio_init(const char *name, u32 mdio_base,
+struct mii_dev *cpsw_mdio_init(const char *name, phys_addr_t mdio_base,
 			       u32 bus_freq, int fck_freq)
 {
 	struct cpsw_mdio *cpsw_mdio;

drivers/net/ti/cpsw_mdio.h
@@ -10,7 +10,7 @@
 
 struct cpsw_mdio;
 
-struct mii_dev *cpsw_mdio_init(const char *name, u32 mdio_base,
+struct mii_dev *cpsw_mdio_init(const char *name, phys_addr_t mdio_base,
 			       u32 bus_freq, int fck_freq);
 void cpsw_mdio_free(struct mii_dev *bus);
 u32 cpsw_mdio_get_alive(struct mii_dev *bus);

commit 14bc77caea6fab628657664646a8e77cce9b88cc

driver: net: ti: cpsw-mdio: Fix compiler warning for 32 bit builds
Below warning is seen on 32 bit builds:

drivers/net/ti/cpsw_mdio.c: In function ‘cpsw_mdio_init’:
drivers/net/ti/cpsw_mdio.c:147:20: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
  cpsw_mdio->regs = (struct cpsw_mdio_regs *)mdio_base;
                    ^
Fix this by using map_physmem() when casting physical address to struct
pointer.

Fixes: 52635614056f ("driver: net: ti: cpsw-mdio: use phys_addr_t for mdio_base addr")
Signed-off-by: Vignesh R <vigneshr@ti.com>

drivers/net/ti/cpsw_mdio.c
@@ -144,7 +144,9 @@ struct mii_dev *cpsw_mdio_init(const char *name, phys_addr_t mdio_base,
 		return NULL;
 	}
 
-	cpsw_mdio->regs = (struct cpsw_mdio_regs *)mdio_base;
+	cpsw_mdio->regs = map_physmem(mdio_base,
+				      sizeof(struct cpsw_mdio_regs),
+				      MAP_NOCACHE);
 
 	if (!bus_freq || !fck_freq)
 		cpsw_mdio->div = CPSW_MDIO_DIV_DEF;