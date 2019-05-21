kaustav-net/ti-uboot

commit 87b60543b78a46fa6cd5287eb3bf89baef91a12b

configs: ti: Factor out environment support commands for FIT loading
Some parts of these commands can be reused, factor out commonly used
sequences and move them to common files.

Signed-off-by: Andrew F. Davis <afd@ti.com>

include/configs/am65x_evm.h
@@ -113,6 +113,7 @@
 		"done;\0"						\
 	"get_kern_mmc=load mmc ${bootpart} ${loadaddr} "		\
 		"${bootdir}/${name_kern}\0"				\
+	"get_fit_mmc=load mmc ${bootpart} ${fit_loadaddr} ${bootdir}/${fit_bootfile}\0" \
 	"partitions=" PARTS_DEFAULT
 
 /* Command for booting the Android from eMMC */
@@ -138,8 +139,7 @@
 		"env delete boot_start\0"				\
 	"emmc_android_boot="						\
 		"echo Trying to boot Android from eMMC ...; "		\
-		"setenv fit_loadaddr 0x90000000; "			\
-		"setenv loadaddr ${fit_loadaddr}; "			\
+		"run update_to_fit; "					\
 		"setenv eval_bootargs setenv bootargs $bootargs; "	\
 		"run eval_bootargs; "					\
 		"setenv mmcdev 0; "					\
@@ -148,11 +148,8 @@
 		"part start mmc ${mmcdev} boot boot_start; "		\
 		"part size mmc ${mmcdev} boot boot_size; "		\
 		"mmc read ${fit_loadaddr} ${boot_start} ${boot_size}; "	\
-		"for overlay in $overlay_files;"			\
-		"do;"							\
-		"setenv overlaystring ${overlaystring}'#'${overlay};"	\
-		"done;"							\
-		"bootm ${fit_loadaddr}#${fdtfile}${overlaystring}\0"
+		"run get_overlaystring; "				\
+		"run run_fit\0"
 
 #ifdef CONFIG_TARGET_AM654_A53_EVM
 #define EXTRA_ENV_AM65X_BOARD_SETTINGS_MTD				\

include/configs/ti_armv7_common.h
@@ -55,7 +55,13 @@
 	"fit_loadaddr=0x90000000\0" \
 	"fit_bootfile=fitImage\0" \
 	"update_to_fit=setenv loadaddr ${fit_loadaddr}; setenv bootfile ${fit_bootfile}\0" \
-	"loadfit=run args_mmc; bootm ${loadaddr}#${fdtfile};\0" \
+	"get_overlaystring=" \
+		"for overlay in $overlay_files;" \
+		"do;" \
+		"setenv overlaystring ${overlaystring}'#'${overlay};" \
+		"done;\0" \
+	"run_fit=bootm ${loadaddr}#${fdtfile}${overlaystring}\0" \
+	"loadfit=run args_mmc; run run_fit;\0" \
 
 /*
  * DDR information.  If the CONFIG_NR_DRAM_BANKS is not defined,

commit 43ecc5ecf42d7ffea5b227a3b13df9f7192012eb

configs: ti: Include DEFAULT_FIT_TI_ARGS in default ENV
The FIT support was factored out into the common DEFAULT_FIT_TI_ARGS
but on AM65x we do not currently include that, include it here.

Fixes: 87b60543b78a ("configs: ti: Factor out environment support commands for FIT loading")
Signed-off-by: Andrew F. Davis <afd@ti.com>
Tested-by: Praneeth Bajjuri <praneeth@ti.com>

include/configs/am65x_evm.h
@@ -176,6 +176,7 @@
 /* Incorporate settings into the U-Boot environment */
 #define CONFIG_EXTRA_ENV_SETTINGS					\
 	DEFAULT_MMC_TI_ARGS						\
+	DEFAULT_FIT_TI_ARGS						\
 	EXTRA_ENV_AM65X_BOARD_SETTINGS					\
 	EXTRA_ENV_AM65X_BOARD_SETTINGS_MMC				\
 	EXTRA_ENV_AM65X_BOARD_SETTINGS_EMMC_ANDROID			\